5b6a91c6417aaba7d8982757714177e0f033212f
bw2789 <bojunw2789@nycstudents.net>
2025-10-27 14:43:00 +0000

    sdaas

--- VRLesson2_5bClasswork/script.js
+++ VRLesson2_5bClasswork/script.js
@@ -1,5 +1,5 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene, snowmen = [ ];
+let scene, snowmen = [ ], cloud = [ ];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
@@ -19,7 +19,13 @@ window.addEventListener("DOMContentLoaded",function() {
   }
 
   //Challenge 1: Create an array to store 20 clouds in random positions
-  cloud = new Cloud(-5,10,-1);
+
+  for(let  i=0; i<20; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let cloud = new Cloud(x,z);
+    cloud.push(cloud);
+  }
 
   //Challenge 3: Create an array to store 100 snowflakes in random positions
 
@@ -30,7 +36,11 @@ function loop(){
   for(let snowman of snowmen){
     snowman.spin();
   }
-  cloud.fly();
+
+  for(let cloud of cloud){
+    cloud.fly();
+  }
+ 
   //Challenge 2: Traverse your array of clouds and make each cloud fly
 
   //Challenge 4: Traverse your array of snowflakes and make each snowflake fall
