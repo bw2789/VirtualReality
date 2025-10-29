99778cf6113421c170e5606be044b46ed1f04b6f
bw2789 <bojunw2789@nycstudents.net>
2025-10-29 14:44:50 +0000

    dsadas

--- VRPractical2_4/script.js
+++ VRPractical2_4/script.js
@@ -4,13 +4,7 @@ let scene, rocket = [ ] ;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
 
-  for(let i = 0; i < 10; i++){
-    let x = rnd(-20,20);
-    let z = rnd(-20,20);
-    let y = rnd(20,25)
-    let rocket = new Rocket(x, y, z);
-    let house = new House(x1, 0 , z1);
-  }
+  rocket = new Rocket(0, 2, 0)
 
   
   
