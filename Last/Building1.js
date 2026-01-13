c9761f7be1f4b2987d01805b6723bce41ca514aa
bw2789 <bojunw2789@nycstudents.net>
2026-01-13 14:58:50 +0000

    added simple frame for door wall for floor1

--- Last/Building1.js
+++ Last/Building1.js
@@ -92,7 +92,7 @@ class Floor1Wall{
 
     let floor1window1 = document.createElement("a-box");
     floor1window1.setAttribute("color","#D7DFD8");
-    floor1window1.setAttribute("opacity","0.3w");
+    floor1window1.setAttribute("opacity","0.3");
     floor1window1.setAttribute("position","5.725 1.6 -7");
     floor1window1.setAttribute("height","4");
     floor1window1.setAttribute("width","2.7");
@@ -102,7 +102,7 @@ class Floor1Wall{
 
     let floor1window2 = document.createElement("a-box");
     floor1window2.setAttribute("color","#D7DFD8");
-    floor1window2.setAttribute("opacity","0.3w");
+    floor1window2.setAttribute("opacity","0.3");
     floor1window2.setAttribute("position","2.725 1.6 -7");
     floor1window2.setAttribute("height","4");
     floor1window2.setAttribute("width","2.7");
@@ -112,7 +112,7 @@ class Floor1Wall{
 
     let floor1window3 = document.createElement("a-box");
     floor1window3.setAttribute("color","#D7DFD8");
-    floor1window3.setAttribute("opacity","0.3w");
+    floor1window3.setAttribute("opacity","0.3");
     floor1window3.setAttribute("position","-0.3 1.6 -7");
     floor1window3.setAttribute("height","4");
     floor1window3.setAttribute("width","2.7");
@@ -122,7 +122,7 @@ class Floor1Wall{
 
     let floor1window4 = document.createElement("a-box");
     floor1window4.setAttribute("color","#D7DFD8");
-    floor1window4.setAttribute("opacity","0.3w");
+    floor1window4.setAttribute("opacity","0.3");
     floor1window4.setAttribute("position","-3.25 1.6 -7");
     floor1window4.setAttribute("height","4");
     floor1window4.setAttribute("width","2.7");
@@ -132,7 +132,7 @@ class Floor1Wall{
 
     let floor1window5 = document.createElement("a-box");
     floor1window5.setAttribute("color","#D7DFD8");
-    floor1window5.setAttribute("opacity","0.3w");
+    floor1window5.setAttribute("opacity","0.3");
     floor1window5.setAttribute("position","-5.925 1.6 -7");
     floor1window5.setAttribute("height","4");
     floor1window5.setAttribute("width","2.3");
@@ -142,6 +142,52 @@ class Floor1Wall{
 
     
     
+
+    this.obj.setAttribute("position",{x:x, y:y, z:z});
+    scene.append( this.obj )
+  }
+}
+
+class Floor1Door{
+  constructor(x,y,z){
+    this.obj = document.createElement("a-entity");
+      
+    // Door implementation
+
+    let door = document.createElement("a-box");
+    door.setAttribute("color","#8B4513");
+    door.setAttribute("position","0 0 0");
+    door.setAttribute("height","2.5");
+    door.setAttribute("width","0.5");
+    door.setAttribute("depth","0.25");
+    this.obj.append( door );
+
+    let floor1wall1 = document.createElement("a-box");
+    floor1wall1.setAttribute("color","#B3B3B3");
+    floor1wall1.setAttribute("position","0 3.6 0");
+    floor1wall1.setAttribute("height","0.3");
+    floor1wall1.setAttribute("width","14.8");
+    floor1wall1.setAttribute("depth","0.25");
+    floor1wall1.setAttribute("side","double");
+    this.obj.append( floor1wall1 );
+
+    let floor1wall2 = document.createElement("a-box");
+    floor1wall2.setAttribute("color","#B3B3B3");
+    floor1wall2.setAttribute("position","-7.25 2.1 0");
+    floor1wall2.setAttribute("height","3.3");
+    floor1wall2.setAttribute("width","0.35");
+    floor1wall2.setAttribute("depth","0.25");
+    floor1wall2.setAttribute("side","double");
+    this.obj.append( floor1wall2 );
+
+    let floor1wall3 = document.createElement("a-box");
+    floor1wall3.setAttribute("color","#B3B3B3");
+    floor1wall3.setAttribute("position","7.25 2.1 0");
+    floor1wall3.setAttribute("height","3.3");
+    floor1wall3.setAttribute("width","0.35");
+    floor1wall3.setAttribute("depth","0.25");
+    floor1wall3.setAttribute("side","double");
+    this.obj.append( floor1wall3 );
 
     this.obj.setAttribute("position",{x:x, y:y, z:z});
     scene.append( this.obj )
@@ -166,6 +212,9 @@ class Building1{
     let wall3 = new Floor1Wall(0,0,0);
     wall3.obj.setAttribute("rotation",{x:0, y:-90, z:0});
     this.obj.append( wall3.obj );
+
+    let door = new Floor1Door(0,0,7.125);
+    this.obj.append( door.obj );
 
     this.obj.setAttribute("position",{x:x, y:y, z:z});
     scene.append( this.obj )
